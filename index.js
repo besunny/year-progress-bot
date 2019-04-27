053b2b91b8719686c1593a4b20b792fc15172cd5
besunny <ssssuniiii@gmail.com>
2019-04-27 21:03:14 +0430

    Added progress bar

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const DayPercentage = require('./percentage.js')
 const token = '860279432:AAGqsuBdxUwrK9JdHA9hC7o5a42KRe3CQtk';
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 // Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, function(msg, match)  {
+bot.onText(/\/echo (.+)/, function (msg, match) {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
@@ -21,29 +21,62 @@ bot.onText(/\/echo (.+)/, function(msg, match)  {
   bot.sendMessage(chatId, resp);
 });
 
-bot.onText(/\/date/, function(msg, match) { 
-	console.log("DONE!")
-	const chatId = msg.chat.id;
-	const showDate = new PersianDate()
-	const date = showDate.format();
-	
-	bot.sendMessage(chatId, 'This Year:'+date);
+bot.onText(/\/start/, function onStart(msg) {
+  const opts = {
+    reply_to_message_id: msg.message_id,
+    reply_markup: JSON.stringify({
+      keyboard: [
+        [{ text: 'Show Today\'s Date' }],
+        [{ text: 'Today\'s Percentage!' }]
+      ],
+      resize_keyboard: true
+    })
+  };
+  bot.sendMessage(msg.chat.id, 'Do you love me?', opts);
+});
+
+bot.onText(/Show Today\'s Date/, function (msg, match) {
+  console.log("DONE!")
+  const chatId = msg.chat.id;
+  const showDate = new PersianDate()
+  const date = showDate.format();
+
+  bot.sendMessage(chatId, 'This Year:' + date);
 }
 )
 
-bot.onText(/\/percentage/, function(msg){
+bot.onText(/Today\'s Percentage!/, function (msg) {
   const chatId = msg.chat.id;
   const percentage = DayPercentage.calculate();
 
-  bot.sendMessage(chatId, 'Today\'s Percentage: '+Math.round(percentage)+'%');
+  bot.sendMessage(chatId, 'Today\'s Percentage:\n' + generateProgressBar(percentage) + ' ' + Math.round(percentage) + '% ');
 }
 )
 
 // Listen for any kind of message. There are different kinds of
 // messages.
-// bot.on('message', function(msg)  {
-//   const chatId = msg.chat.id;
-//   // console.log(msg)
-//   // send a message to the chat acknowledging receipt of their message
-//   bot.sendMessage(chatId, 'Received your message');
-// });
+bot.on('message', function (msg) {
+  const chatId = msg.chat.id;
+  console.log(msg)
+  // send a message to the chat acknowledging receipt of their message
+  // bot.sendMessage(chatId, 'Received your message');
+});
+
+
+
+function generateProgressBar(percentage) {
+  var x = Math.floor(20 * percentage / 100);
+  var y = 20 - x;
+  var a = '';
+    console.log(x, percentage)
+  for (var i = 0; i < x; i++) {
+    a += "⣿"
+  }
+
+  for (var j = 0; j < y; j++) {
+    a += "⣀"
+  }
+
+  return a;
+}
+
